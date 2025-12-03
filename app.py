724cfc4b627eaa3ce3b2b84ce894f60363a8b8f8
Vishnu Vardhan Reddy <162047840+medamvishnu27@users.noreply.github.com>
2025-12-03 09:53:26 +0530

    Update CORS origins for frontend deployment

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 # CORS configuration
 CORS(
     app,
-    origins=["http://localhost:5173", "https://vehicle-damage-dl-frontend.onrender.com", "*"],
+    origins=["https://vd-dlproject.vercel.app/", "*"],
     supports_credentials=True,
     allow_headers=["Content-Type", "Authorization"],
     methods=["GET", "POST", "OPTIONS"]
